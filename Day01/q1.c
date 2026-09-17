2968f8f6b2f6093fec77992e6c00a2d472ddbd0a
sanikaaa5 <Sanika.42652@stu.upes.ac.in>
2026-09-17 17:58:42 +0000

    Day01

---
+++ Day01/q1.c
@@ -0,0 +1,23 @@
+/*
+ * Name     : Sanika
+ * Roll no  : 590042652
+ * Day : 01 Question : 1
+ * Date : 17-09-26
+ * PROBLEM STATEMENT:
+ * Write a program to input two numbers and display their sum.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
