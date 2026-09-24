50554873534c088fc4e742a6e4f97fb3e40ec97a
sanikaaa5 <Sanika.42652@stu.upes.ac.in>
2026-09-24 20:13:02 +0000

    Day23

---
+++ Day23/q1.c
@@ -0,0 +1,30 @@
+/*
+ * Name : Sanika
+ * Roll no. : 590042652
+ * Day : 23 Question: 1
+ * Date : 25-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15
+ + ... up to n terms.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + (float)(2 * i) / (4 * i - 1);
+    }
+
+    printf("Sum = %.2f", sum);
+
+    return 0;
+}
